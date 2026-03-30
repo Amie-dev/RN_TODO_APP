671a86c9cbff6f78c708e039da21bfc76afd7fce
amie-dev <learn.amie01@gmail.com>
2026-03-30 15:49:41 +0530

    add authScreen

--- src/screen/AuthScreen/AuthScreen.js
+++ src/screen/AuthScreen/AuthScreen.js
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import LogIn from '../../components/AuthComponents/LogIn';
+import GetStar from '../../components/AuthComponents/GetStar';
+import SingUp from '../../components/AuthComponents/SingUp';
 
 const AuthScreen = () => {
+  const [step, setStep] = useState(0);   // 0 = GetStart, 1 = Auth
+  const [login, setLogin] = useState(false); // false = Signup, true = Login
+
   return (
-    <View>
-      <Text>AuthScreen</Text>
-    </View>
-  )
-}
+    <>
+      {/* 🔥 Step 0 → Get Started */}
+      {step === 0 && (
+        <GetStar step={step} setStep={setStep} />
+      )}
 
-export default AuthScreen
+      {/* 🔥 Step 1 → Auth Screen */}
+      {step >0 && (
+       ! login ? (
+          <LogIn login={login} setLogin={setLogin} />
+        ) : (
+          <SingUp login={login} setLogin={setLogin} />
+        )
+      )}
+    </>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default AuthScreen;
+
+const styles = StyleSheet.create({});
