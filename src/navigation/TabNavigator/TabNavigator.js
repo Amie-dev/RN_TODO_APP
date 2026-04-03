839ad2aa41db003bf76c953548541b8bd0213bcb
amie-dev <learn.amie01@gmail.com>
2026-04-03 21:24:58 +0530

    add ICON in tab

--- src/navigation/TabNavigator/TabNavigator.js
+++ src/navigation/TabNavigator/TabNavigator.js
@@ -1,35 +1,45 @@
+/* eslint-disable react/no-unstable-nested-components */
 import { View, Text } from 'react-native';
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeStack from '../StackNavigator/HomeStack';
 import TodoStack from '../StackNavigator/TodoStack';
 import ProfileStack from '../StackNavigator/ProfileStack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+// import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const Tab = createBottomTabNavigator();
+
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="Home"
-        component={HomeStack}
-        options={{
-          headerShown: false,
-        }}
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } 
+          else if (route.name === 'Todo') {
+            iconName = focused ? 'list' : 'list-outline';
+          } 
+          else if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+
+        tabBarActiveTintColor: '#99ae0f',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeStack}
       />
-      <Tab.Screen
-        name="Todo"
-        component={TodoStack}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={ProfileStack}
-        options={{
-          headerShown: false,
-        }}
-      />
+      <Tab.Screen name="Todo" component={TodoStack} />
+      <Tab.Screen name="Profile" component={ProfileStack} />
     </Tab.Navigator>
   );
 };
